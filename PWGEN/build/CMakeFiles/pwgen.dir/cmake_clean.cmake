ba54a784ad3d4428a5078261cbb9163c22c473d9
MtNite <112702620+mttmrz@users.noreply.github.com>
2023-07-20 01:23:13 +0200

    Add files via upload

---
+++ PWGEN/build/CMakeFiles/pwgen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pwgen.dir/pwgen.cpp.o"
+  "pwgen"
+  "pwgen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pwgen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
